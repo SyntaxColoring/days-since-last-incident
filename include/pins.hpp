36e3c3a953614ad1bf0b80dd79c6152efb768d7d
Max Marrone <max@marrone.nyc>
2023-08-23 23:27:37 -0400

    Rearrange pins to avoid interfering with USB programming.

--- include/pins.hpp
+++ include/pins.hpp
@@ -7,31 +7,31 @@ namespace pins
 	// The pin for the button.
 	// The button should be normally open,
 	// and the other side should be connected to GND.
-	const static unsigned BUTTON = A1;
+	const static unsigned BUTTON = A3;
 
 	// The pin for the speaker.
 	// The other side of the speaker should be connected to GND.
 	// There should be a 220R resistor somewhere in that series,
 	// to protect both the speaker and the MCU pin.
-	const static unsigned SPEAKER = A2;
+	const static unsigned SPEAKER = A4;
 
 	// Pins for each segment of each 7-segment display.
 	// Each segment should have a 100R resistor between it and the pin.
 	// The common anode of the display should be connected to VIN,
 	// which should have +9V from the barrel jack power supply.
 	const static SevenSegmentPinMap TENS_DIGIT = {
-		0,
-		1,
+		// Start at pin 2 to avoid RX and TX on 0 and 1, which apparently
+		// interfere with programming.
 		2,
 		3,
 		4,
 		5,
 		6,
+		7,
+		8,
 	};
 
 	const static SevenSegmentPinMap ONES_DIGIT = {
-		7,
-		8,
 		9,
 		10,
 		11,
@@ -39,5 +39,7 @@ namespace pins
 		// Skip digital pin 13 because it has the board's builtin LED.
 		// If we also put our segment on that pin, it would be too dim.
 		A0,
+		A1,
+		A2,
 	};
 }
